b8ecf2b306226575f08fb2f6782b8eb12c196ab0
admin <kuligturan@mail.ri>
2021-10-04 18:53:14 +0600

    deploy heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e@kzhebpmh@*88_%&7%17f^8hyqe@j@2_gemv3sve$re7mscdj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
